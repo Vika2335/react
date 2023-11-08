674b11a0097d8bada488e2e29f2c4971a77613e1
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2023-11-08 18:26:25 +0300

    Убрал отступы дурацкие

--- src/App.js
+++ src/App.js
@@ -4,27 +4,21 @@ import Image from "./components/Image"
 import logo from "./img/logo.png"
 
 class App extends React.Component {
-        helpText = "Help text!"
-        render() {
-            return ( < div className = "name" >
-                <
-                Header title = "Шапка сайта" / >
-                <
-                h1 > { this.helpText } < /h1>    <
-                input placeholder = { this.helpText }
-                onClick = { this.inputClick }
-                onMouseEnter = { this.mouseOver }
-                />    <
-                p > { this.helpText == "Help text!" ? "Yes" : "NO" } < /p>   <
-                Image image = { logo }
-                />  <
-                img src = { logo }
-                /> <
-                /div>)
-            }
+  helpText = "Help text!"
+  render(){
+    return (<div className = "name">
+      <Header title="Шапка сайта"/>
+      <h1> { this.helpText } </h1>   
+      <input placeholder = { this.helpText }
+        onClick = { this.inputClick } onMouseEnter = { this.mouseOver }/>   
+      <p> { this.helpText == "Help text!" ? "Yes" : "NO" } </p>  
+      <Image image={logo}/> 
+      <img src={logo}/>
+    </div>)
+  }
 
-            inputClick() { console.log("Clicked") }
-            mouseOver() { console.log("Mouse Over") }
-        }
+  inputClick(){ console.log("Clicked") }
+  mouseOver(){ console.log("Mouse Over") }
+}
 
-        export default App
+export default App
