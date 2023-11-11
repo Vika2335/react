b5838c7b67c93ddce4e4a07e51550e078954fe43
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2023-11-11 18:00:26 +0300

    Стили, состояния

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from "react"
 import Header from "./components/Header"
 import Image from "./components/Image"
-import logo from "./img/logo.png"
+import logo from "./img/react-logo.png"
 
 class App extends React.Component {
-  helpText = "Help text!"
+  constructor(props){
+    super(props);
+    this.state = {
+      helpText: "Помоги выучить React!",
+      userData: ""
+    }
+
+    this.inputClick = this.inputClick.bind(this)
+  }
+
   render(){
     return (<div className = "name">
-      <Header title="Шапка сайта"/>
-      <h1> { this.helpText } </h1>   
-      <input placeholder = { this.helpText }
+      <Header title="ReacT"/>
+      <h1> { this.state.helpText } </h1>   
+      <h2>{ this.state.userData }</h2>
+      <input placeholder = { this.state.helpText }
+        onChange={event => this.setState({userData: event.target.value})}
         onClick = { this.inputClick } onMouseEnter = { this.mouseOver }/>   
-      <p> { this.helpText == "Help text!" ? "Yes" : "NO" } </p>  
+      <p> { this.state.helpText == "Помоги выучить React!" ? "NO" : "Yes" } </p>  
       <Image image={logo}/> 
-      <img src={logo}/>
+      <img src={logo} className="image"/>
     </div>)
   }
 
-  inputClick(){ console.log("Clicked") }
+  inputClick(){ 
+    this.setState({ helpText: "Спасибо!" })
+    console.log("Clicked") 
+  }
   mouseOver(){ console.log("Mouse Over") }
 }
 
