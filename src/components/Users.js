6baa83720c3c37db01fb4967f6e9669dd3502fe7
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2023-11-14 20:44:30 +0300

    react

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,34 +1,37 @@
 import React from "react"
+import User from "./User"
 
 class Users extends React.Component {
-    users = [
-        {
-            id: 1,
-            firstname: 'Viktoria',
-            lastname: 'Golobokova',
-            bio: 'React',
-            age: 19,
-            isHappy: true
-        },
-        {
-            id: 2,
-            firstname: 'Merlin',
-            lastname: 'Vale',
-            bio: 'Food',
-            age: 24,
-            isHappy: false
+    constructor(props){
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    firstname: 'Viktoria',
+                    lastname: 'Golobokova',
+                    bio: 'React',
+                    age: 19,
+                    isHappy: true
+                },
+                {
+                    id: 2,
+                    firstname: 'Merlin',
+                    lastname: 'Vale',
+                    bio: 'Food',
+                    age: 24,
+                    isHappy: false
+                }
+        
+            ]
         }
-
-    ]
-    
+    }
     render(){
-        if(this.users.length > 0)
+        if(this.state.users.length > 0)
             return(<div>
-                {this.users.map((el) => (<div className="user" key={el.id}>
-                    <h3>{el.firstname} {el.lastname}</h3>
-                    <p>{el.bio}</p>
-                    <b>{el.isHappy ? 'Счастлив)' : 'Не особо('}</b>
-                </div>))}
+                {this.state.users.map((el) => (
+                    <User key={el.id} user={el}/>
+                ))}
             </div>)
         else
             return(<div className="user">
