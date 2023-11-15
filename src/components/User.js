22d0457b34ddef28f24f1ea217e38370e5a8f282
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2023-11-15 14:51:48 +0300

    Починила и сломала

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,14 @@
 import React from "react"
-import
+import { IoCloseCircleSharp } from 'react-icons/io5'
+import { BsFeather } from "react-icons/bs";
 
 class User extends React.Component {
     user = this.props.user
     render(){
         return(
             <div className="user">
+                <IoCloseCircleSharp className="delete"/>
+                <BsFeather className="edit"/>
                 <h3>{this.user.firstname} {this.user.lastname}</h3>
                 <p>{this.user.bio}</p>
                 <b>{this.user.isHappy ? 'Счастлив)' : 'Не особо('}</b>
