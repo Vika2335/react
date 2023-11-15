852355a2a7ff045fadaea1d9168ebc942ec0ee8b
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2023-11-15 19:03:10 +0300

    Удаление и редактирование записей

--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,30 @@
 import React from "react"
+import AddUser from "./AddUser";
 import { IoCloseCircleSharp } from 'react-icons/io5'
 import { BsFeather } from "react-icons/bs";
 
 class User extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            editForm: false
+        }
+    }
     user = this.props.user
     render(){
         return(
             <div className="user">
-                <IoCloseCircleSharp className="delete"/>
-                <BsFeather className="edit"/>
+                <IoCloseCircleSharp onClick={() => this.props.onDelete(this.user.id)} className="delete"/>
+                <BsFeather onClick={() => {
+                    this.setState({
+                        editForm: !this.state.editForm
+                    })
+                }} className="edit"/>
                 <h3>{this.user.firstname} {this.user.lastname}</h3>
                 <p>{this.user.bio}</p>
                 <b>{this.user.isHappy ? 'Счастлив)' : 'Не особо('}</b>
+
+                {this.state.editForm && <AddUser user={this.user} onAdd={this.props.onEdit} />}
             </div>
         )
     }
